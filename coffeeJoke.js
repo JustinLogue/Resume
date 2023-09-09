331282c603b31f5b6b9c3f60fed26da0480d9e7c
justin <justin.noname@gmail.com>
2023-09-09 12:43:42 -0400

    list displayer

---
+++ coffeeJoke.js
@@ -0,0 +1,38 @@
+
+let cupsOfCoffeeDrunk =0
+
+if (cupsOfCoffeeDrunk <= 3) {
+    drinkMoreCoffee();
+    cupsOfCoffeeDrunk ++;
+}
+else{
+    alert("STOP! You're going to give yourself a Heart Attack!");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
